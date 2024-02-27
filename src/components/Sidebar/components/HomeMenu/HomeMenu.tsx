7369dbddbcd013e02617218f7cdc5756d41bb331
ahsan-chy <ahsan.chy.iqbal@gmail.com>
2024-02-27 22:32:07 +0500

    [Animation] Slider animation on change of home collections

--- src/components/Sidebar/components/HomeMenu/HomeMenu.tsx
+++ src/components/Sidebar/components/HomeMenu/HomeMenu.tsx
@@ -9,13 +9,15 @@ interface HomeMenuProps {
 const HomeMenu: React.FC<HomeMenuProps> = ({ collectionNav, handleActive }) => {
   return (
     <>
-      {collectionNav.map((navData) => (
+      {collectionNav && collectionNav.map((navData) => (
         <div
-          className="nav-item collection-nav"
+          className={`${
+            navData.status === true ? "active-right-nav" : "de-active-right"
+          } nav-item collection-nav`}
           onClick={() => handleActive(navData)}
           key={navData.id}>
           <img src={navData.mainImage} alt="" />
-          <div className={`${navData.status === true ? "active-right-nav" : "de-active-right"}`}>
+          <div>
             <p>{navData.title}</p>
             <h3>{navData.heading}</h3>
           </div>
